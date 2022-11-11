6e6ce5f4fa711d633cbc6961afcbfdce2aa9e457
Jasvir12 <kaur.jasvir101997@gmail.com>
2022-11-11 13:06:07 +0530

    html

---
+++ CONTACT FORM/contact.php
@@ -0,0 +1,144 @@
+<?php
+
+ 
+
+
+
+$saved=0;
+session_start();
+
+
+$servername="localhost";
+
+$admin="root";
+$password="";
+
+$db="astro";
+
+
+$conn=mysqli_connect($servername,$admin,$password,$db);
+if(!$conn)
+{
+die("connection failed".mysqli_connect_error());
+}
+
+$name=$email=$subject=$message='';
+if(isset($_POST['submit']))
+	{
+	
+
+	
+
+
+	$name=$_POST['name'];
+    $email=$_POST['email'];
+	$subject=$_POST['subject'];
+    $message=$_POST['message'];
+	
+	
+	
+	
+	
+
+$quaryup="INSERT INTO `astro`.`contact` (`name`, `email`, `subject`,`message`) VALUES ('$name', '$email', '$subject','$message')";
+
+
+
+
+
+if(mysqli_query($conn,$quaryup))
+{  
+
+
+//$p=1;
+	
+	echo"data saved";
+}
+  
+  
+	else
+	{
+		echo"<br>data not saved".mysqli_error($conn);
+			
+	}
+      
+
+}    
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 data-aos="fade-up">Contact</h2>
+          <p data-aos="fade-up">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row justify-content-center">
+
+          <div class="col-xl-3 col-lg-4 mt-4" data-aos="fade-up">
+            <div class="info-box">
+              <i class="bx bx-map"></i>
+              <h3>Our Address</h3>
+              <p>Home Town Hoshiarpur,Punjab,Pin code-146001</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-lg-4 mt-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="info-box">
+              <i class="bx bx-envelope"></i>
+              <h3>Email Us</h3>
+              <p>wadhawangurjeet@gmail.com</p>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-4 mt-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="info-box">
+              <i class="bx bx-phone-call"></i>
+              <h3>Call Us</h3>
+              <p>+91-7526995410</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="300">
+          <div class="col-xl-9 col-lg-12 mt-4">
+            <form action="" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div><br/>
+                            <div class="text-center"><input type="Submit" name="submit"></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+</body>
+</html>
---
+++ day18/action_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+Submitted Form Data
+Your input was received as:
+fname=John&lname=Doe 
+The server has processed your input and returned this answer.
+
+Note: This tutorial will not teach you how servers are processing input. Processing input is explained in our PHP tutorial.
+?>
+</body>
+</html>
